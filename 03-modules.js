6ec395e1c6dac3cffdabfcdf9cefdc51ade693e4
celeis <gonca.negis@gmail.com>
2021-11-16 22:51:25 +0300

    first commit

---
+++ 03-modules.js
@@ -0,0 +1,8 @@
+const { john, peter } = require("./04-names");
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+require("./07-mind-grenade");
+
+// sayHi("susan");
+// sayHi(john);
+// sayHi(peter);
